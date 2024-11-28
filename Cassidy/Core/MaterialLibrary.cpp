02db50b9aa607c95bea4cf9550a343bf2c55c1f0
Gregor McQueen <mcqueengregor@gmail.com>
2024-11-28 19:30:13 +0000

    Moved material library files, updated CMakeLists.txt

---
+++ Cassidy/Core/MaterialLibrary.cpp
@@ -0,0 +1,20 @@
+#include "MaterialLibrary.h"
+#include <iostream>
+
+cassidy::Material* MaterialLibrary::buildMaterialImpl(const std::string& materialName, const cassidy::MaterialInfo& materialInfo)
+{
+  // If material already exists, return reference to it:
+  if (m_materialCache.find(materialName) != m_materialCache.end())
+  {
+    std::cout << "Material " << materialName << " has already been built!" << std::endl;
+    return &m_materialCache.at(materialName);
+  }
+
+  cassidy::Material newMat;
+
+  /* 
+    - Get descriptor layout from descriptor builder layout cache
+    - Descriptor builder creates descriptor set using available descriptor pool, descriptor layout and material info
+  */
+
+}
