b8bbde2a7d8fd5636ae6cdd5b7bad97539e0859f
Gregor McQueen <mcqueengregor@gmail.com>
2025-06-15 01:06:58 +0100

    Updated CMake build to include spdlog

--- Cassidy/main.cpp
+++ Cassidy/main.cpp
@@ -1,7 +1,13 @@
 #include "Core/Engine.h"
+#include <spdlog/spdlog.h>
 
 int main(int argc, char* argv[])
 {
+  spdlog::info("Hello, world!");
+  spdlog::error("This is an error.");
+  spdlog::debug("This is a debug message.");
+  spdlog::warn("This is a warning message!");
+
   cassidy::Engine engine(glm::uvec2(1280, 720));
 
   engine.init();
